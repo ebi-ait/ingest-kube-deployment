627239b3ec6756551283e1f3def847ebb51b7db5
Victor <victor73@github.com>
2018-02-02 22:01:17 -0500

    More changes related to running script without github ssh keys.

--- setup.sh
+++ setup.sh
@@ -5,8 +5,20 @@ git clone https://github.com/HumanCellAtlas/ingest-core.git
 git clone https://github.com/HumanCellAtlas/ingest-broker.git
 git clone https://github.com/HumanCellAtlas/ingest-accessioner.git
 git clone https://github.com/HumanCellAtlas/ingest-validator.git
-git clone --recursive https://github.com/HumanCellAtlas/ingest-exporter.git
-git clone --recursive https://github.com/HumanCellAtlas/ingest-staging-manager.git
+
+# The following 2 repos make use of git submodules that would normally require
+# a git clone with a --recursive flag. However, the submodules are defined with
+# github URLs that require ssh keys. Here we work around that by obtaining the
+# code and cloning it into an "ingestbroker" subdirectory.
+git clone https://github.com/HumanCellAtlas/ingest-exporter.git && \
+    pushd ingest-exporter && \
+    git clone http://github.com/HumanCellAtlas/ingest-broker.git ingestbroker && \
+    popd
+git clone https://github.com/HumanCellAtlas/ingest-staging-manager.git && \
+    pushd ingest-staging-manager && \
+    git clone https://github.com/HumanCellAtlas/ingest-broker.git ingestbroker && \
+    popd
+
 # build images
 cd ingest-core
 docker build -t ingest-core:local .
@@ -20,6 +32,7 @@ cd ../ingest-exporter
 docker build -t ingest-exporter:local .
 cd ../ingest-staging-manager
 docker build -t ingest-staging-manager:local .
+
 # Apply Kubernetes configs
 cd ../ingest-kube-deployment/local-dev/ingestion
 kubectl apply -f service-rabbit-deploy.yml
@@ -34,9 +47,13 @@ kubectl apply -f ingest-accessioner-deploy.yml
 kubectl apply -f ingest-validator-deploy.yml
 kubectl apply -f ingest-exporter-deploy.yml
 kubectl apply -f ingest-staging-manager-deploy.yml
-# Minikube Dashboard
+
+# Minikube Dashboard. This invoke's the user's default browser to the dashboard's
+# URL.
 minikube dashboard
+
 # Check Ingest API
 minikube service ingest-core-service --url
+
 # Check Demo UP
 minikube service ingest-demo-service --url
