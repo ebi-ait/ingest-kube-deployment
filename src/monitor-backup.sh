2eea6ff2fb212f40db0c9711b60b6cbf59a9f18d
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2018-03-28 15:53:56 +0100

    Corrected conditional flow.

--- src/monitor-backup.sh
+++ src/monitor-backup.sh
@@ -3,12 +3,13 @@
 inotifywait -qr -e modify --format '%w%f' /data/sync/ | while read FILE
 do 
     if [ -z "$S3_BUCKET" ]; then
-	if [ -z "$BACKUP_DIR"  ]; then
-	    aws s3 cp $FILE s3://$S3_BUCKET/$BACKUP_DIR/
-	else
-	    aws s3 cp $FILE s3://$S3_BUCKET
-	fi
-    else
 	echo "S3 Bucket not specified!"
+    else
+	if [ -z "$BACKUP_DIR"  ]; then
+	    aws s3 cp $FILE s3://$S3_BUCKET
+	else
+	    aws s3 cp $FILE s3://$S3_BUCKET/$BACKUP_DIR/
+	    
+	fi
     fi
 done
