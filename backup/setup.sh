700c883d34b894848e37fbb46fb8da984f37d559
Rodrey Goite <rodrey@ebi.ac.uk>
2019-03-28 14:11:42 +0000

    Defined setup script for Ingest backup.

---
+++ backup/setup.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+if [ -z $DEPLOYMENT_STAGE ]; then
+    echo "Unspecified DEPLOYMENT_STAGE env variable."
+    exit 1
+else
+    echo "INFO: using deployment stage [${DEPLOYMENT_STAGE}]"
+fi
+
+if [ -z $1  ]; then
+    echo "No AWS credentials file specified."
+    exit 1
+fi
+
+IFS="," 
+read -ra credentials <<< $(awk -F "," 'NR==2' $1)
+
+access_key=$(echo ${credentials[0]} | openssl base64)
+secret_access_key=$(echo ${credentials[1]} | openssl base64)
+
+helm upgrade --wait --install -f backup/${DEPLOYMENT_STAGE}.yaml --set secret.aws.accessKey=${access_key},secret.aws.secretAccessKey=${secret_access_key} ingest-backup backup
+
+
+
+
+
+
