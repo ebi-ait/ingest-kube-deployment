845879b0050cda11d52b8e71c5d79dbe53ecf4d6
Rodrey Goite <rodrey@ebi.ac.uk>
2019-05-16 10:59:41 +0100

    Updated find-image script to expect command.

--- release/find-image.sh
+++ release/find-image.sh
@@ -1,22 +1,26 @@
 #!/usr/bin/env bash
 
-if [[ -z $1 || -z $2 ]]; then
-    echo 'Expecting deployment and version arguments.'
+if [[ -z $1 || -z $2 || -z $3 ]]; then
+    echo 'Invalid arguments. Expecting command, deployment, and search key.'
     exit 1
 fi
 
 src_dir=$(dirname ${BASH_SOURCE[0]})
 
-deployment=$1
-version=$2
+command=$1
+deployment=$2
+search_key=$3
 
 function has_match() {
-    cat ${src_dir}/version.map | grep -qE '^'${deployment}'\b.*\b'${version}'.*$'
+    cat ${src_dir}/version.map | grep -qE '^'${deployment}'\b.*\b'${search_key}'.*$'
 }
 
+function version() {
+    if has_match; then
+       cat ${src_dir}/version.map | grep -E '^'${deployment}'\b.*\b'${search_key}'.*$' | awk '{printf("%s -> %s\n", $3, $2)}'
+    else
+	echo "No results found for [${deployment}]."
+    fi
+}
 
-if has_match; then
-    cat ${src_dir}/version.map | grep -E '^'${deployment}'\b.*\b'${version}'.*$' | awk '{printf("%s -> %s\n", $3, $2)}'
-else
-    echo "No results found for [${deployment}]."
-fi
+$command
