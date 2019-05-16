516cc46f1e7f9e386a9e98f7effe969df0afc07a
Rodrey Goite <rodrey@ebi.ac.uk>
2019-05-16 11:48:23 +0100

    Added display modes to the script to only show image or version.

--- release/find.sh
+++ release/find.sh
@@ -1,26 +1,62 @@
 #!/usr/bin/env bash
 
-if [[ -z $1 || -z $2 || -z $3 ]]; then
-    echo 'Invalid arguments. Expecting command, deployment, and search key.'
+ARGS=()
+display_mode="default"
+
+for arg in "$@"; do
+    case $arg in
+	-v|--version)
+	    if [[ "${display_mode}" != "default" ]]; then
+		echo "Multiple display modes selected."
+		exit 1
+	    fi
+	    display_mode=version
+	    shift
+	    ;;
+	-i|--image)
+	    if [[ "${display_mode}" != "default" ]]; then
+		echo "Multiple display modes selected."
+		exit 1
+	    fi
+	    display_mode=image
+	    shift
+	    ;;
+	*)
+	    ARGS+=("$1")
+	    shift
+	    ;;
+    esac
+done
+
+set -- "${ARGS[@]}"
+if [[ -z $1 || -z $2 ]]; then
+    echo 'Invalid arguments. Expecting deployment, and search key.'
     exit 1
 fi
 
 src_dir=$(dirname ${BASH_SOURCE[0]})
 
-command=$1
-deployment=$2
-search_key=$3
+deployment=$1
+search_key=$2
 
 function has_match() {
     cat ${src_dir}/version.map | grep -qE '^'${deployment}'\b.*\b'${search_key}'.*$'
 }
 
-function version() {
-    if has_match; then
-       cat ${src_dir}/version.map | grep -E '^'${deployment}'\b.*\b'${search_key}'.*$' | awk '{printf("%s -> %s\n", $3, $2)}'
-    else
-	echo "No results found for '${search_key}' in [${deployment}]."
-    fi
-}
+if has_match; then
+    case $display_mode in
+	version)
+	    awk_command='{printf("%s\n", $3)}'
+	    ;;
+	image)
+	    awk_command='{printf("%s\n", $2)}'
+	    ;;
+	*)
+	    awk_command='{printf("%s -> %s\n", $3, $2)}'
+	    ;;
+    esac
+    cat ${src_dir}/version.map | grep -E '^'${deployment}'\b.*\b'${search_key}'.*$' | awk "${awk_command}"
+else
+    echo "No results found for '${search_key}' in [${deployment}]."
+fi
 
-$command
