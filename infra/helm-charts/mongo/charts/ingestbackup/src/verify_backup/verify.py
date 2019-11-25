513e0229e5d2dc7cdc3f1856671edb2aa3ce3984
Rolando Fernandez <rolando@ebi.ac.uk>
2019-11-25 16:10:30 +0000

    big fix

--- infra/helm-charts/mongo/charts/ingestbackup/src/verify_backup/verify.py
+++ infra/helm-charts/mongo/charts/ingestbackup/src/verify_backup/verify.py
@@ -6,7 +6,6 @@ from os import environ
 from pymongo import MongoClient
 from reporting import SlackHandler
 
-$S3_BUCKET/$BACKUP_DIR
 backup_dir = f'{environ.get("S3_BACKUP")}/{environ.get("BACKUP_DIR")}'
 mongo_host = environ.get('MONGO_HOST', 'mongo')
 mongo_port_env = environ.get('MONGO_PORT', '27017')
