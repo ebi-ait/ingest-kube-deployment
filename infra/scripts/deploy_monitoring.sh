b1f0f2c75872ad711b61c91e066602beb06a7cfc
Jacob Windsor <me@jcbwndsr.com>
2021-05-19 18:23:40 +0200

    Allow upgrade

--- infra/scripts/deploy_monitoring.sh
+++ infra/scripts/deploy_monitoring.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
 prometheus_IP=$(kubectl get svc -n gitlab-managed-apps -o jsonpath="{.items[?(@.metadata.name=='prometheus-prometheus-server')].spec.clusterIP}")
+
 login_username=$(kubectl get secret aws-keys -o jsonpath="{.data.grafana-admin-username}" | base64 --decode)
 login_password=$(kubectl get secret aws-keys -o jsonpath="{.data.grafana-admin-password}" | base64 --decode)
 login_url="https://monitoring.ingest.${DEPLOYMENT_STAGE}.archive.data.humancellatlas.org"
+
 if [ $DEPLOYMENT_STAGE == "prod" ]
 then
     login_url="https://monitoring.ingest.archive.data.humancellatlas.org"
 fi
 
-source ../config/environment_$DEPLOYMENT_STAGE &&\
-helm upgrade ingest-monitoring helm-charts/ingest-monitoring\
-    --set prometheusIP=$prometheus_IP\
-    --force --install
-
+source ../config/environment_$DEPLOYMENT_STAGE
+if [[ $* == '--upgrade' ]]
+then
+    echo "Performing upgrade..."
+    helm upgrade ingest-monitoring helm-charts/ingest-monitoring\
+        --set prometheusIP=$prometheus_IP\
+        --values helm-charts/ingest-monitoring/values.yaml\
+        --values helm-charts/ingest-monitoring/environments/$DEPLOYMENT_STAGE.yaml\
+        --install
+else
+    helm upgrade ingest-monitoring helm-charts/ingest-monitoring\
+        --set prometheusIP=$prometheus_IP\
+        --values helm-charts/ingest-monitoring/values.yaml\
+        --values helm-charts/ingest-monitoring/environments/$DEPLOYMENT_STAGE.yaml\
+        --force --install
+fi
 
 
 
