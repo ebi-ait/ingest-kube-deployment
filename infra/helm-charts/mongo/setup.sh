675984b225cfbce72b5c3a6c0aff3d9acc488acf
Jacob Windsor <me@jcbwndsr.com>
2021-09-17 12:50:57 +0200

    Try new values

--- infra/helm-charts/mongo/setup.sh
+++ infra/helm-charts/mongo/setup.sh
@@ -1,17 +1,27 @@
 #! /usr/bin/env sh
 
-deployment_stage=$1
+if [ -z $DEPLOYMENT_STAGE ]; then
+    echo "Unspecified DEPLOYMENT_STAGE env variable."
+    exit 1
+else
+    echo "INFO: using deployment stage [${DEPLOYMENT_STAGE}]"
+fi
 
-s3_access_secrets=$(aws secretsmanager get-secret-value --secret-id ingest/${deployment_stage}/mongo-backup/s3-access  --region=us-east-1 | jq -r .SecretString)
+secret=ingest/${DEPLOYMENT_STAGE}/mongo-backup/s3-access
+secret=$(aws --region us-east-1 secretsmanager get-secret-value\
+                 --secret-id $secret\
+                 --query SecretString\
+                 --output text 2> /dev/null)
 
-access_key_id=$(echo ${s3_access_secrets} | jq -jr .access_key_id | base64)
-access_key_secret=$(echo ${s3_access_secrets} | jq -jr .access_key_secret| base64)
-slack_alert_webhook=$(aws secretsmanager get-secret-value --secret-id ingest/${deployment_stage}/alerts  --region=us-east-1 | jq -r .SecretString | jq -r .webhook_url)
+access_key_id=$(echo $secret | jq -jr .access_key_id | base64 --decode)
+access_key_secret=$(echo $secret | jq -jr .access_key_secret| base64 --decode)
+
+# slack_alert_webhook=$(aws secretsmanager get-secret-value --secret-id ingest/${DEPLOYMENT_STAGE}/alerts  --region=us-east-1 | jq -r .SecretString | jq -r .webhook_url)
 
 helm package .
 
-. ../../../config/environment_${deployment_stage}
+# . ../../../config/environment_${DEPLOYMENT_STAGE}
 
-helm upgrade mongo ./ --wait --install --force -f values.yaml -f environments/${deployment_stage}.yaml --set ingestbackup.secret.aws.accessKey=${access_key_id},ingestbackup.secret.aws.secretAccessKey=${access_key_secret},ingestbackup.verification.slack.webhookUrl=${slack_alert_webhook}
+helm upgrade mongo ./ --wait --install --force -f values.yaml -f environments/${DEPLOYMENT_STAGE}.yaml --set ingestbackup.secret.aws.secretAccessKey=${access_key_secret},ingestbackup.verification.slack.webhookUrl=${slack_alert_webhook}
 
 rm *.tgz
