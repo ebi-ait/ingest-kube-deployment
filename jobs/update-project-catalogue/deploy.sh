d0283a1dfad8378c1d41062c0b2be01ab4548afa
Jacob Windsor <me@jcbwndsr.com>
2021-10-15 11:20:15 +0100

    Allow deploy script to be ran from anywhere

--- jobs/update-project-catalogue/deploy.sh
+++ jobs/update-project-catalogue/deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-source ../../config/environment_$DEPLOYMENT_STAGE
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+source $SCRIPT_DIR/../../config/environment_$DEPLOYMENT_STAGE
 
 helm upgrade update-project-catalogue .\
         --values ./values.yaml\
