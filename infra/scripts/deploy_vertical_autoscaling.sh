a4ed5d8954f4892a7f675d9c9b0ebfca1afff88f
Jacob Windsor <me@jcbwndsr.com>
2022-04-07 16:35:50 +0200

    dcp-651 use prometheus and deploy updater

--- infra/scripts/deploy_vertical_autoscaling.sh
+++ infra/scripts/deploy_vertical_autoscaling.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+prometheus_IP=$(kubectl get svc -n gitlab-managed-apps -o jsonpath="{.items[?(@.metadata.name=='prometheus-prometheus-server')].spec.clusterIP}")
+
 cd helm-charts
 helm dependency update vertical-autoscaling && helm package vertical-autoscaling
 
 if [[ $* == '--upgrade' ]]
 then
-    helm upgrade --values vertical-autoscaling/values.yaml vertical-autoscaling ./vertical-autoscaling --install
+    helm upgrade --values vertical-autoscaling/values.yaml --set vpa.recommender.extraArgs.prometheus-address=$prometheus_IP vertical-autoscaling ./vertical-autoscaling --install
 else
-    helm upgrade --values vertical-autoscaling/values.yaml vertical-autoscaling ./vertical-autoscaling --install --force
+    helm upgrade --values vertical-autoscaling/values.yaml --set vpa.recommender.extraArgs.prometheus-address=$prometheus_IP vertical-autoscaling ./vertical-autoscaling --install --force
 fi
 
 rm *.tgz
