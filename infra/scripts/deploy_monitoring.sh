42fac4f7e74ac994e059b1c03ee428a5c3f68308
Jacob Windsor <me@jcbwndsr.com>
2022-05-26 17:17:35 +0200

    dcp-775 dont use environment files for ingest-monitoring

--- infra/scripts/deploy_monitoring.sh
+++ infra/scripts/deploy_monitoring.sh
@@ -41,12 +41,12 @@ then
     echo "Performing upgrade..."
     helm upgrade ingest-monitoring helm-charts/ingest-monitoring\
         --values helm-charts/ingest-monitoring/values.yaml\
-        --values helm-charts/ingest-monitoring/environments/$DEPLOYMENT_STAGE.yaml\
+        --set url=$login_url
         --install
 else
     helm upgrade ingest-monitoring helm-charts/ingest-monitoring\
         --values helm-charts/ingest-monitoring/values.yaml\
-        --values helm-charts/ingest-monitoring/environments/$DEPLOYMENT_STAGE.yaml\
+        --set url=$login_url
         --force --install
 fi
 
