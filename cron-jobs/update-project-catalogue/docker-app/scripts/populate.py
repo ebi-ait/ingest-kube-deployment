d2dc4a77e72427ec76cac6bd5de1b1e4850d15ca
Amnon Khen <amnon@ebi.ac.uk>
2025-08-18 17:48:58 +0100

    increase resources for gitlab runner

--- cron-jobs/update-project-catalogue/docker-app/scripts/populate.py
+++ cron-jobs/update-project-catalogue/docker-app/scripts/populate.py
@@ -1,9 +1,12 @@
 import argparse, json, requests, logging, os
+from collections import Counter
+from enum import Enum
+
 
 def is_in_dcp(uuid):
     azul_proj_url = f'https://service.azul.data.humancellatlas.org/index/projects/{uuid}'
     if requests.get(azul_proj_url):
-        logging.info(f'Project {uuid} is in DCP')
+        logging.info(f'Project {uuid} is published in DCP')
         return True
     return False
 
@@ -16,7 +19,7 @@ def get_publications_journal(project):
     for publication in publications:
         try:
             if not publication.get('doi'):
-                logging.info(f"Project {project['uuid']['uuid']} has no doi")
+                logging.info(f"Project {project['uuid']['uuid']} has no doi. Skipping.")
                 continue
 
             r = requests.get(f"https://api.crossref.org/works/{publication['doi']}")
@@ -62,11 +65,17 @@ def perform_patch(patch, url, token=None):
     r = requests.patch(url, data=data, headers=headers)
     r.raise_for_status()
 
-def patch_project(uuid, project, token=None):
+class UpdateStatus(Enum):
+    SUCCESS=1
+    UNNECESSARY=2
+    FAIL=3
+
+
+def patch_project(uuid, project, token=None) -> UpdateStatus:
     try:
         update_url = f'{project["_links"]["self"]["href"]}?partial=true'
         project_patch = {}
-        
+
         if project["wranglingState"] != "Published in DCP" and is_in_dcp(uuid):
             project_patch["wranglingState"] = "Published in DCP"
 
@@ -75,7 +84,7 @@ def patch_project(uuid, project, token=None):
 
             if "publicationsInfo" not in project or publications_info != project['publicationsInfo'] and len(publications_info) > 0:
                 project_patch["publicationsInfo"] = publications_info
-                
+
                 # Remove all publicationsInfo first
                 # Circumvents bug where authors are appended if they already exist
                 # See dcp-363
@@ -84,13 +93,14 @@ def patch_project(uuid, project, token=None):
 
         if not project_patch:
             logging.info(f"Patch unnecessary for project UUID: {uuid}")
+            return UpdateStatus.UNNECESSARY
         else:
             perform_patch(project_patch, update_url, token)
             logging.info(f"Successfully updated project {uuid} with patch: {project_patch}")
-
+            return UpdateStatus.SUCCESS
     except Exception as e:
         logging.error(f'Error patching project {uuid}: {e}')
-
+        return UpdateStatus.FAIL
 
 def get_uuids_from_catalogue(base_url, count):
     catalogue_url = f'{base_url}/projects/search/catalogue?page=0&size={count}&sort=cataloguedDate,desc'
@@ -99,10 +109,22 @@ def get_uuids_from_catalogue(base_url, count):
     for project in res.json()['_embedded']['projects']:
         yield project['uuid']['uuid']
 
+
 def get_token(input_file):
     with open(input_file, "r") as file:
         return file.read()
-        
+
+def resolve_ingest_url():
+    if args.url:
+        url = args.url
+    else:
+        # Build the url from internal service IP
+        # See: https://kubernetes.io/docs/concepts/services-networking/connect-applications-service/#environment-variables
+        local_core_ip = os.getenv("INGEST_CORE_SERVICE_SERVICE_HOST")
+        local_core_port = int(os.getenv("INGEST_CORE_SERVICE_SERVICE_PORT"))
+        url = f'http://{local_core_ip}:{local_core_port}'
+    return url
+
 
 if __name__ == "__main__":
     logging.basicConfig()
@@ -111,31 +133,29 @@ if __name__ == "__main__":
     description = "Update projects in ingest that are in the catalogue to have the correct wrangling status if they are in the DCP"
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument("-c", "--count", help="Number of projects to pull to check for updates", default=50, type=int)
-    
+
     # Args for running this script locally
     parser.add_argument("-u", "--url", help="Base URL for Ingest API", default=None)
     parser.add_argument("-t", "--token", help="Text file containing an ingest token. Used for local testing purposes", default=None)
     parser.add_argument("-i", "--uuid", help="Comma seperated list of UUIDs to use instead of querying the catalogue. Used for testing", default=None)
-
+    logging.info('started update-project-catalogue')
     args =  args = parser.parse_args()
-    
-    if args.url:
-        url = args.url
-    else:
-        # Build the url from internal service IP
-        # See: https://kubernetes.io/docs/concepts/services-networking/connect-applications-service/#environment-variables
-        local_core_ip = os.getenv("INGEST_CORE_SERVICE_SERVICE_HOST")
-        local_core_port = int(os.getenv("INGEST_CORE_SERVICE_SERVICE_PORT"))
-        url = f'http://{ local_core_ip }:{ local_core_port }'
+
+    url = resolve_ingest_url()
 
     logging.info(f'Using base url {url}')
 
     token = get_token(args.token) if args.token else None
     uuids = args.uuid.split(",") if args.uuid else get_uuids_from_catalogue(url, args.count)
-    
+
     logging.info(f'Querying {args.count} projects...')
+
+    update_results = Counter()
 
     for uuid in uuids:
         project = get_project(uuid, url)
         if project:
-            patch_project(uuid, project, token)
+            update_status = patch_project(uuid, project, token)
+            update_results[update_status.name]+=1
+    logging.info(f'update results: {update_results}')
+    logging.info('finished update-project-catalogue')
